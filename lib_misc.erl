ecf27eb25ae88c510e9c1df27997165b6ef3a399
hujimori <hujimori16@gmail.com>
2021-03-23 22:48:02 +0900

    p.44まで読了

--- lib_misc.erl
+++ lib_misc.erl
@@ -2,6 +2,8 @@
 -export([sum/1]).
 -export([for/3]).
 -export([qsort/1]).
+-export([pythag/1]).
+-export([perms/1]).
 
 sum(L) -> sum(L,0).
 
@@ -16,3 +18,15 @@ qsort([Pivot|T]) ->
     qsort([X || X <- T, X < Pivot])
     ++ [Pivot] ++
     qsort([X || X <- T, X >= Pivot]).
+
+pythag(N) ->
+    [{A,B,C} ||
+        A <- lists:seq(1,N),
+        B <- lists:seq(1,N),
+        C <- lists:seq(1,N),
+        A+B+C =< N,
+        A*A+B*B =:= C*C
+    ].
+
+perms([]) -> [[]];
+perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
